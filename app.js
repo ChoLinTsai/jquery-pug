a5396a195e5d449e6909c9329f226552f73c4336
Max Tsai <maxtsai@ISTWLAPAPPLE1602.local>
2018-09-25 11:07:50 +0800

    update ticks; packages updated; config updated

--- app.js
+++ app.js
@@ -1,58 +1,74 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
+const createError = require('http-errors');
+const express = require('express');
+const session = require('express-session');
+const fs = require('fs')
+const path = require('path')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+// const fileUpload = require('express-fileupload');
+const multer = require('multer');
 
-const indexRouter = require("./routes/index");
-const apiRouter = require("./routes/api");
-const authRouter = require("./routes/auth");
+
+// import routers
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
+const fileRouter = require('./routes/file');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
 
+app.locals.moment = require('moment');
+
 // Database
-const mysql = require("mysql");
+const mysql = require('mysql');
 const con = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
+  host: '127.0.0.1',
+  user: 'root',
   port: 13306,
-  password: "12345",
-  database: "users"
+  password: '12345',
+  database: 'users'
 });
 
 con.connect(function (err) {
   if (err) {
-    console.log("connecting error");
+    console.log('connecting error');
     return;
   }
-  console.log("connecting success");
+  console.log('connecting success');
 });
 
 // view engine setup
-app.set("views", path.join(__dirname, "src/views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'pug');
 
 app.use(cors({
   origin: ['http://127.0.0.1:4000', 'http://localhost:3000'],
   credentials: true
 }));
 
-app.use(logger("dev"));
+// use configs
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// express session
-app.use(express.static(path.join(__dirname, "src")));
 
-// Authentication and Authorization Middleware
-var auth = function (req, res, next) {
-  if (req.session && req.session.user === "amy" && req.session.admin)
-    return next();
-  else return res.sendStatus(401);
-};
+
+// express session
+app.set('trust proxy', 1)
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: { maxAge: 60000000000 },
+}))
+
+
+// setting middleware
+app.use('/jquery', express.static(__dirname + '/Jquery'));
+
+app.use(express.static(path.join(__dirname, 'src')));
+
 
 // db state
 app.use(function (req, res, next) {
@@ -60,9 +76,10 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/api", apiRouter);
-app.use("/auth", authRouter);
-app.use("/", indexRouter);
+app.use('/api', apiRouter);
+app.use('/file', fileRouter);
+app.use('/auth', authRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -73,18 +90,22 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 // Node server
 // Constants
 const PORT = 4000;
-const HOST = "127.0.0.1";
-app.listen(PORT, HOST);
+const HOST = '127.0.0.1';
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, HOST);
+}
+
 console.log(`Running on http://${HOST}:${PORT}`);
 
 module.exports = app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const poststylus = require('poststylus')
@@ -10,17 +10,11 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   entry: {
     main: './src/js/main.js',
-    getAllUserData: './src/js/getAllUserData.js'
+    getAllUserData: './src/js/getAllUserData.js',
+    modals: './src/js/modals.js',
   },
   output: {
     path: path.resolve(__dirname, "docs/")
-  },
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/
-  },
-  resolve: {
-    extensions: [".js", ".css", ".styl"]
   },
   module: {
     rules: [
@@ -34,12 +28,26 @@ module.exports = {
         ]
       },
       {
-        test: /\.styl$/,
-        use: [
-          "style-loader",
-          'css-loader',
-          'stylus-loader'
-        ]
+        test: /\.pug/,
+        use: 'pug-loader'
+      },
+      {
+        test: [/\.styl$/, /\.css$/],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            },
+            {
+              loader: 'stylus-loader'
+            }
+          ],
+          publicPath: __dirname + '/dist'
+        })
       },
       {
         test: /\.(png|jpg|gif|svg)$/i,
@@ -77,20 +85,26 @@ module.exports = {
     port: 9090,
   },
   optimization: {
-
+    minimize: true
   },
   plugins: [
     new HtmlWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "style.css"
+    new ExtractTextPlugin({
+      filename: "./src/style/styles.bundle.css",
+      allChunks: true
     }),
-    // new webpack.ProvidePlugin({
-    //   $: 'jquery',
-    //   jQuery: 'jquery',
-    //   'window.jQuery': 'jquery'
+    // new MiniCssExtractPlugin({
+    //   filename: "[name].css",
+    //   chunkFilename: "[id].css"
     // }),
+    new webpack.ProvidePlugin({
+      // $: 'jquery',
+      // jQuery: 'jquery',
+      // 'window.jQuery': 'jquery',
+      // moment: 'moment'
+    }),
     new webpack.LoaderOptionsPlugin({
-      test: /\.styl$/,
+      test: /\.styl$/i,
       stylus: {
         default: {
           use: [poststylus()],
